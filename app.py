6ce936f111b1b23d280bf900cdaefc7af2dc8abd
Roberto G. Fortes <52727243+robertofortes23@users.noreply.github.com>
2023-05-05 20:56:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,183 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import json
+import sqlite3
+import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DATABASE = 'quiz.db'
 
-db = SQLAlchemy(app)
 
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    alternatives = db.Column(db.String(255), nullable=False)
-    Q = db.Column(db.String(255), nullable=False)
-    correct = db.Column(db.Integer, nullable=False)
-    explanation = db.Column(db.String(255), nullable=False)
-    referTo = db.Column(db.String(255), nullable=False)
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS quiz (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                alternatives TEXT,
+                question_text TEXT,
+                correct INTEGER,
+                explanation TEXT,
+                refer_to TEXT,
+                subject TEXT,
+                year INTEGER,
+                topic TEXT
+            );
+        """)
 
-    def __init__(self, alternatives, Q, correct, explanation, referTo):
-        self.alternatives = alternatives
-        self.Q = Q
-        self.correct = correct
-        self.explanation = explanation
-        self.referTo = referTo
 
-@app.route('/questions', methods=['GET'])
-def get_questions():
-    questions = Question.query.all()
-    return jsonify([{"alternatives": q.alternatives, "Q": q.Q, "correct": q.correct, "explanation": q.explanation, "referTo": q.referTo} for q in questions])
-
-@app.route('/questions', methods=['POST'])
+@app.route('/quiz', methods=['POST'])
 def add_question():
     data = request.json
-    new_question = Question(data['alternatives'], data['Q'], data['correct'], data['explanation'], data['referTo'])
-    db.session.add(new_question)
-    db.session.commit()
-    return jsonify({"message": "Question added successfully!"})
+    alternatives = data.get('alternatives')
+    alternatives_str = ",".join(alternatives)
+    question_text = data.get('question_text')
+    correct = data.get('correct')
+    explanation = data.get('explanation')
+    refer_to = data.get('refer_to')
+    subject = data.get('subject')
+    year = data.get('year')
+    topic = data.get('topic')
 
-@app.route('/questions/<int:id>', methods=['PATCH'])
-def update_question(id):
-    question = Question.query.get(id)
-    data = request.json
-    question.alternatives = data['alternatives']
-    question.Q = data['Q']
-    question.correct = data['correct']
-    question.explanation = data['explanation']
-    question.referTo = data['referTo']
-    db.session.commit()
-    return jsonify({"message": "Question updated successfully!"})
+    try:
+        with sqlite3.connect(DATABASE) as conn:
+            conn.execute("""
+                INSERT INTO quiz (alternatives, question_text, correct, explanation, refer_to, subject, year, topic)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+            """, (alternatives_str, question_text, correct, explanation, refer_to, subject, year, topic))
+    except sqlite3.Error as e:
+        return jsonify({'error': str(e)}), 400
 
-@app.route('/questions/<int:id>', methods=['DELETE'])
+    return jsonify({'message': 'Question added successfully'}), 201
+
+@app.route('/quiz', methods=['GET'])
+def get_questions():
+    subject = request.args.get('subject')
+    year = request.args.get('year')
+    topic = request.args.get('topic')
+    randomize = request.args.get('random') == 'true'
+
+    with sqlite3.connect(DATABASE) as conn:
+        if subject and year and topic:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE subject = ? AND year = ? AND topic = ?;
+            """, (subject, year, topic))
+        elif subject and topic:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE subject = ? AND topic = ?;
+            """, (subject, topic))
+        elif year and topic:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE year = ? AND topic = ?;
+            """, (year, topic))
+        elif subject and year:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE subject = ? AND year = ?;
+            """, (subject, year))
+        elif subject:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE subject = ?;
+            """, (subject,))
+        elif year:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE year = ?;
+            """, (year,))
+        elif topic:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz WHERE topic = ?;
+            """, (topic,))
+        else:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT * FROM quiz;
+            """)
+        data = cursor.fetchall()
+
+    questions = []
+    for row in data:
+        question = {
+            'id': row[0],
+            'alternatives': row[1].split(","),
+            'question_text': row[2],
+            'correct': row[3],
+            'explanation': row[4],
+            'refer_to': row[5],
+            'subject': row[6],
+            'year': row[7],
+            'topic': row[8]
+        }
+        questions.append(question)
+
+    if randomize:
+        random.shuffle(questions)
+
+    return jsonify(questions)
+
+
+@app.route('/quiz/<int:id>', methods=['DELETE'])
 def delete_question(id):
-    question = Question.query.get(id)
-    db.session.delete(question)
-    db.session.commit()
-    return jsonify({"message": "Question deleted successfully!"})
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM quiz WHERE id = ?', (id,))
+        question = cursor.fetchone()
+        if question is None:
+            return jsonify({'error': 'Question not found'}), 404
+        conn.execute('DELETE FROM quiz WHERE id = ?', (id,))
+        conn.commit()
 
+    return jsonify({'message': 'Question deleted'})
+
+@app.route('/quiz/<int:id>', methods=['PATCH'])
+def update_question(id):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM quiz WHERE id = ?', (id,))
+        question = cursor.fetchone()
+        if question is None:
+            return jsonify({'error': 'Question not found'}), 404
+
+        data = request.json
+        alternatives = data.get('alternatives')
+        if alternatives:
+            alternatives_str = ",".join(alternatives)
+            conn.execute('UPDATE quiz SET alternatives = ? WHERE id = ?', (alternatives_str, id))
+
+        question_text = data.get('question_text')
+        if question_text:
+            conn.execute('UPDATE quiz SET question_text = ? WHERE id = ?', (question_text, id))
+
+        correct = data.get('correct')
+        if correct:
+            conn.execute('UPDATE quiz SET correct = ? WHERE id = ?', (correct, id))
+
+        explanation = data.get('explanation')
+        if explanation:
+            conn.execute('UPDATE quiz SET explanation = ? WHERE id = ?', (explanation, id))
+
+        refer_to = data.get('refer_to')
+        if refer_to:
+            conn.execute('UPDATE quiz SET refer_to = ? WHERE id = ?', (refer_to, id))
+
+        subject = data.get('subject')
+        if subject:
+            conn.execute('UPDATE quiz SET subject = ? WHERE id = ?', (subject, id))
+
+        year = data.get('year')
+        if year:
+            conn.execute('UPDATE quiz SET year = ? WHERE id = ?', (year, id))
+
+        topic = data.get('topic')
+        if topic:
+            conn.execute('UPDATE quiz SET topic = ? WHERE id = ?', (topic, id))
+
+        conn.commit()
+
+    return jsonify({'message': 'Question updated successfully'}), 200
+    
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
